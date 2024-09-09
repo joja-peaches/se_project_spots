13bd7dfd2336f52bf92f8240d2fc19ea5ecfedce
joja_peaches <joja.peaches7@gmail.com>
2024-09-09 11:25:13 -0500

    Add modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,28 @@
-const initialCards = [object1, object2, object3, object4, object5, object6];
-
-const object1 {
+const initialCards = [
+{
   name: "Venice Beach Skate Park",
   link: "https://i.imgur.com/DNBmqc0.jpeg}",
-};
-
-const object2 {
+},
+{
   name: "Drinks in downtown LA",
   link: "https://i.imgur.com/adQIaGo.jpeg",
-};
-
-const object3 {
+},
+{
   name: "East Side of Los Angeles",
   link: "https://i.imgur.com/atPkbSV.jpeg",
-};
-
-const object4 {
+},
+{
   name: "Mutato Muzika record production",
   link: "https://i.imgur.com/H1yfTHh.jpeg",
-};
-
-const object5 {
+},
+{
   name: "Southern California beaches",
   link: "https://i.imgur.com/LGEkmhf.jpeg",
-};
+},
 
-const object6 {
+{
   name: "Beautiful cars in Tuningen, Germany",
   link: "https://i.imgur.covm/CFcBAd2.jpeg",
-};
+},
+];
+console.log(initialCards);
